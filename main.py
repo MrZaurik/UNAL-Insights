e0da1109ec8c3165f917f443399dbbe088f14c2e
Juan G. Saurith M. <juanguisam@gmail.com>
2024-02-29 00:05:17 -0500

    Actualización del Cambio en el Aside. Así como la mejora en la visualización del sitio.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import dash_bootstrap_components as dbc
 import pandas as pd
 import plotly.express as px
+import dash
 
 from dash import Dash, html, dcc, callback, Output, Input
 
@@ -17,7 +18,6 @@ paletaColores = ['#0a9396', '#94d2bd', '#e9d8a6',
                  '#ee9b00', '#ca6702', '#bb3e03', '#ae2012']
 
 # Get the card component
-# create reusable card component
 
 
 def get_card_component(title, data):
@@ -51,11 +51,20 @@ app.layout = dbc.Container(
                                      '''),
                         html.Hr(),
 
-                        # Aquí puedes agregar componentes adicionales para el Sidebar
-                        dbc.Checkbox(id='checkbox1', label='Opción 1'),
-                        dbc.Checkbox(id='checkbox2', label='Opción 2'),
-                        dbc.Checkbox(id='checkbox3', label='Opción 3'),
-                        # ...otros componentes del Sidebar...
+                        # Botones para cambiar entre "Main" y "Custom"
+                        html.Div(
+                            [
+                                dbc.Button("Main", id="btn-main",
+                                           color="primary"),
+                                dbc.Button(
+                                    "Custom", id="btn-custom", color="info"),
+                            ],
+                            style={
+                                'display': 'flex', 'justify-content': 'center', 'margin': '20px 0', 'gap': '10px'}
+                        ),
+
+                        # Nuevo componente para mostrar el texto o los checkboxes
+                        html.Div(id='content-display')
                     ],
                     width=3,  # Ancho del Sidebar (30%)
                     style={'background-color': '#EEE', 'padding': '20px',
@@ -99,12 +108,30 @@ app.layout = dbc.Container(
                                         style={'margin-top': '20px'}
                                     ),
                                     dcc.Graph(
-                                        figure={}, id="score-distribution-histogram")
+                                        figure={}, id="score-distribution-histogram"),
                                 ],
                             )
                         ),
+
+                        # Nuevos gráficos para el panel de visualizaciones
+                        dbc.Row(
+                            [
+                                dbc.Col(
+                                    dcc.Graph(
+                                        figure={}, id="violin-estrato-papa")
+                                ),
+                                dbc.Col(
+                                    dcc.Graph(
+                                        figure={}, id="pie-chart-genero")
+                                ),
+                            ],
+                            style={'margin-top': '20px'}
+                        ),
+
+
                     ],
-                    width=9  # Ancho de la sección de gráficos (70%)
+                    width=9,  # Ancho de la sección de gráficos (70%)
+                    style={'padding': '40px'}
                 ),
             ]
         )
@@ -113,14 +140,132 @@ app.layout = dbc.Container(
     style={'background': '#FBFBFB', 'height': '100vh'}
 )
 
+# Callback para cambiar entre las secciones "Main" y "Custom"
 
-@callback(
+
+@app.callback(
     Output("score-distribution-histogram", "figure"),
-    Input("score-distribution-radios", "value")
+    Output("content-display", "children"),
+    Output("btn-main", "color"),
+    Output("btn-custom", "color"),
+    Input("btn-main", "n_clicks"),
+    Input("btn-custom", "n_clicks")
 )
-def update_histogram(value):
-    figure = px.histogram(df, x=value, color_discrete_sequence=['#0a9396'])
-    return figure
+def update_section(btn_main_clicks, btn_custom_clicks):
+    ctx = dash.callback_context
+    if not ctx.triggered_id:
+        button_id = "btn-main"
+    else:
+        button_id = ctx.triggered_id
+
+    if button_id == "btn-main":
+        # Lógica para la sección "Main"
+        figure = px.scatter()  # Figura vacía para "Main"
+        content = html.P("Texto aleatorio para la sección 'Main'")
+        btn_main_color = "primary"
+        btn_custom_color = "info"
+    else:
+        # Lógica para la sección "Custom"
+        figure = px.histogram(
+            df, x='PAPA', color_discrete_sequence=['#0a9396'])
+
+        # Nuevo contenido con checkboxes para la sección 'Custom'
+        content = html.Div(
+            [
+                html.H3("Programa Curricular"),
+                dbc.Row(
+                    [
+                        dbc.Col(dbc.Checkbox(
+                            label='Biología', id='checkbox-1')),
+                        dbc.Col(dbc.Checkbox(
+                            label='Estadística', id='checkbox-2')),
+                        dbc.Col(dbc.Checkbox(
+                            label='Ing. Mecatrónica', id='checkbox-3')),
+                        dbc.Col(dbc.Checkbox(
+                            label='Ing. Biológica', id='checkbox-4')),
+                        dbc.Col(dbc.Checkbox(
+                            label='Geografía', id='checkbox-5')),
+                        dbc.Col(dbc.Checkbox(
+                            label='Gestión Cultural', id='checkbox-6')),
+                    ],
+                    style={'margin-bottom': '10px'}
+                ),
+
+                html.H3("Género"),
+                dcc.Dropdown(
+                    id='dropdown-genero',
+                    options=[
+                        {'label': 'Hombres', 'value': 'hombres'},
+                        {'label': 'Mujeres', 'value': 'mujeres'},
+                    ],
+                    multi=True,
+                    value=[]
+                ),
+
+                html.H3("Periodo de Ingreso"),
+                dcc.Dropdown(
+                    id='dropdown-periodo-ingreso',
+                    options=[
+                        {'label': '2019-2S', 'value': 'opcion1'},
+                        {'label': '2020-1S', 'value': 'opcion2'},
+                        {'label': '2020-2S', 'value': 'opcion3'},
+                        {'label': '2021-1S', 'value': 'opcion4'},
+                        {'label': '2021-2S', 'value': 'opcion5'},
+                        {'label': '2022-1S', 'value': 'opcion6'},
+                        {'label': '2022-2S', 'value': 'opcion7'},
+                        {'label': '2023-1S', 'value': 'opcion8'},
+                        {'label': '2023-2S', 'value': 'opcion9'},
+                        {'label': '2024-1S', 'value': 'opcion10'}
+                    ],
+                    multi=True,
+                    value=['opcion10']
+                ),
+            ],
+            style={'padding': '20px'}
+        )
+
+        btn_main_color = "info"
+        btn_custom_color = "primary"
+
+    return figure, content, btn_main_color, btn_custom_color
+
+# Callback para el gráfico de Violin entre Estrato Socioeconómico y PAPA
+
+
+@app.callback(
+    Output("violin-estrato-papa", "figure"),
+    Input("btn-custom", "n_clicks")
+)
+def update_violin_estrato_papa(btn_custom_clicks):
+    if btn_custom_clicks:
+        figure = px.violin(
+            df, y="PAPA", x="ESTRATO",
+            color_discrete_sequence=['#0a9396'],
+            box=True, points="all"
+        )
+        figure.update_layout(
+            title="Violin Plot: Estrato Socioeconómico vs PAPA",
+            xaxis_title="Estrato Socioeconómico",
+            yaxis_title="PAPA"
+        )
+        return figure
+    else:
+        return px.scatter()
+
+# Callback para el gráfico de Diagrama Circular con información de género
+
+
+@app.callback(
+    Output("pie-chart-genero", "figure"),
+    Input("btn-custom", "n_clicks")
+)
+def update_pie_chart_genero(btn_custom_clicks):
+    if btn_custom_clicks:
+        figure = px.pie(
+            df, names='GENERO', title='Distribución de Género')
+        return figure
+    else:
+        return px.scatter()
 
 
 # Ejecutar la aplicación
